ce1544d9069863b6dbf3ced85b727284f425679a
HelloOO7 <cendarehor@gmail.com>
2022-11-12 19:33:26 +0100

    Make printing non-inline, move EXL_DESMUME to CMake variable.

--- Util/exl_Print.cpp
+++ Util/exl_Print.cpp
@@ -5,12 +5,36 @@
 #include <stdarg.h>
 #include "exl_Print.h"
 
+#ifdef EXL_PLATFORM_GFL
+
+#ifdef EXL_DESMUME
+
+INLINE void __exlDesmumePrint(const char* str) {
+    asm("MOVS R0, %0" : : "r" (str)); //the compiler may not recognize the need to keep R0 intact when inlining this method
+    asm("SWI 0xFC");
+}
+
+#define __EXL_PRINT(str) __exlDesmumePrint(str)
+#else
+#define __EXL_PRINT(str)
+#endif
+#elif defined(EXL_PLATFORM_WIN32)
+#include "stdio.h"
+#define __EXL_PRINT(str) printf(str)
+#else
+#define __EXL_PRINT(str)
+#endif
+
+void exlPrint(const char* str) {
+    __EXL_PRINT(str);
+}
+
 void exlPrintf(const char* format, ...) {
     va_list args;
     va_start(args, format);
     char outBuffer[1024];
     vsnprintf(outBuffer, 1024, format, args);
-    __EXL_PRINT(outBuffer);
+    exlPrint(outBuffer); //make sure R0 is in the proper place
     va_end(args);
 }
 
--- Util/exl_Print.h
+++ Util/exl_Print.h
@@ -5,29 +5,7 @@
 #include "exl_DllExport.h"
 #include <stdarg.h>
 
-#ifdef EXL_PLATFORM_GFL
-
-#ifdef EXL_DESMUME
-
-INLINE void __exlDesmumePrint(const char* str) {
-    asm("MOV R0, %0" : : "r" (str));
-    asm("SWI 0xFC");
-}
-
-#define __EXL_PRINT(str) __exlDesmumePrint(str)
-#else
-#define __EXL_PRINT(str)
-#endif
-#elif defined(EXL_PLATFORM_WIN32)
-#include "stdio.h"
-#define __EXL_PRINT(str) printf(str)
-#else
-#define __EXL_PRINT(str)
-#endif
-
-INLINE void exlPrint(const char* string) {
-    __EXL_PRINT(string);
-}
+EXL_PUBLIC void exlPrint(const char* string);
 
 EXL_PUBLIC void exlPrintf(const char* format, ...);
 
